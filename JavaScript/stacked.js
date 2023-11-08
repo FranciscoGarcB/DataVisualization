5bc95c33409bf03ea0e84d81afd7090b724a0e3b
Francisco García <franciscogarcb@outlook.com>
2023-11-08 22:53:34 +0100

    Update stacked.js

--- JavaScript/stacked.js
+++ JavaScript/stacked.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 var margin = { top: 10, right: 180, bottom: 90, left:50 },
-width = 960 - margin.left - margin.right,
+width = 1000 - margin.left - margin.right,
 height = 600 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
