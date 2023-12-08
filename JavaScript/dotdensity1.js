a35d95bd4fee67d1b0dd498c2ae726107f6148e5
Francisco García <franciscogarcb@outlook.com>
2023-12-09 00:18:36 +0100

    changed things

--- JavaScript/dotdensity1.js
+++ JavaScript/dotdensity1.js
@@ -1,5 +1,5 @@
 // Reemplaza 'TU_TOKEN_MAPBOX' con tu token de Mapbox
-mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmNpc2NvZ2FyY2IiLCJhIjoiY2xweDN5ZXMwMGxoNDJxbzl4ZTg4Mjg4aiJ9.zmi1_1GQonBXdbnESnCy6g';
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmNpc2NvZ2FyY2IiLCJhIjoiY2xweDN5ZXMwMGxoNDJxbzl4ZTg4Mjg4aiJ9.zmi1_1GQonBXdbnESnCy6g';
 
 // Inicializamos el mapa en una ubicación específica (coordenadas del centro de Estados Unidos)
 var map = new mapboxgl.Map({
@@ -9,33 +9,34 @@ var map = new mapboxgl.Map({
   zoom: 3
 });
 
-// Cargamos los datos desde el archivo CSV
-fetch('../datasets/dot-density.csv')
-  .then(response => response.text())
-  .then(csvData => {
-    // Parseamos el CSV
-    var points = Papa.parse(csvData, { header: true }).data;
+  // Cargamos los datos desde el archivo CSV
+  fetch('../datasets/dot-density.csv')
+    .then(response => response.text())
+    .then(csvData => {
+      // Parseamos el CSV
+      var points = Papa.parse(csvData, { header: true }).data;
 
-    // Convertimos los puntos a GeoJSON
-    var geojson = {
-      type: 'FeatureCollection',
-      features: points.map(point => {
-        var latitude = parseFloat(point.latitude_coordinate);
-        var longitude = parseFloat(point.longitude_coordinate);
+      // Convertimos los puntos a GeoJSON
+      var geojson = {
+        type: 'FeatureCollection',
+        features: points.map(point => {
+          var latitude = parseFloat(point.latitude_coordinate);
+          var longitude = parseFloat(point.longitude_coordinate);
 
-        if (!isNaN(latitude) && !isNaN(longitude)) {
-          return {
-            type: 'Feature',
-            properties: {},
-            geometry: {
-              type: 'Point',
-              coordinates: [longitude, latitude]
-            }
-          };
-        }
-      }).filter(Boolean) // Eliminamos elementos nulos
-    };
+          if (!isNaN(latitude) && !isNaN(longitude)) {
+            return {
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+              }
+            };
+          }
+        }).filter(Boolean) // Eliminamos elementos nulos
+      };
 
+<<<<<<< HEAD
     // Añadimos los datos al mapa
     map.on('load', function() {
       map.addLayer({
@@ -49,6 +50,21 @@ fetch('../datasets/dot-density.csv')
           'circle-radius': 3,
           'circle-color': '#04AA6D'
         }
+=======
+      // Añadimos los datos al mapa
+      map.on('load', function() {
+        map.addLayer({
+          id: 'points',
+          type: 'circle',
+          source: {
+            type: 'geojson',
+            data: geojson
+          },
+          paint: {
+            'circle-radius': 3,
+            'circle-color': '#FF0000'
+          }
+        });
+>>>>>>> parent of ab86a1f (Added gitignore)
       });
     });
-  });
---
+++ apiKey.js
@@ -0,0 +1,1 @@
+const api_Key = 'pk.eyJ1IjoiZnJhbmNpc2NvZ2FyY2IiLCJhIjoiY2xweDN5ZXMwMGxoNDJxbzl4ZTg4Mjg4aiJ9.zmi1_1GQonBXdbnESnCy6g';
