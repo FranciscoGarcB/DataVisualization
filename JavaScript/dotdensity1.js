ab86a1ff43baf36d4e88023374cad92a42d36c11
Francisco García <franciscogarcb@outlook.com>
2023-12-08 23:53:41 +0100

    Added gitignore

--- JavaScript/dotdensity1.js
+++ JavaScript/dotdensity1.js
@@ -1,59 +1,54 @@
-const config = require('./config');
-
-// Now you can access the API key
-const apiKey = config.apiKey;
-
 // Reemplaza 'TU_TOKEN_MAPBOX' con tu token de Mapbox
-  mapboxgl.accessToken = apiKey;
+mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmNpc2NvZ2FyY2IiLCJhIjoiY2xweDN5ZXMwMGxoNDJxbzl4ZTg4Mjg4aiJ9.zmi1_1GQonBXdbnESnCy6g';
 
-  // Inicializamos el mapa en una ubicación específica (coordenadas del centro de Estados Unidos)
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-98.5795, 39.8283],
-    zoom: 4
-  });
+// Inicializamos el mapa en una ubicación específica (coordenadas del centro de Estados Unidos)
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-98.5795, 39.8283],
+  zoom: 4
+});
 
-  // Cargamos los datos desde el archivo CSV
-  fetch('../datasets/dot-density.csv')
-    .then(response => response.text())
-    .then(csvData => {
-      // Parseamos el CSV
-      var points = Papa.parse(csvData, { header: true }).data;
+// Cargamos los datos desde el archivo CSV
+fetch('../datasets/dot-density.csv')
+  .then(response => response.text())
+  .then(csvData => {
+    // Parseamos el CSV
+    var points = Papa.parse(csvData, { header: true }).data;
 
-      // Convertimos los puntos a GeoJSON
-      var geojson = {
-        type: 'FeatureCollection',
-        features: points.map(point => {
-          var latitude = parseFloat(point.latitude_coordinate);
-          var longitude = parseFloat(point.longitude_coordinate);
+    // Convertimos los puntos a GeoJSON
+    var geojson = {
+      type: 'FeatureCollection',
+      features: points.map(point => {
+        var latitude = parseFloat(point.latitude_coordinate);
+        var longitude = parseFloat(point.longitude_coordinate);
 
-          if (!isNaN(latitude) && !isNaN(longitude)) {
-            return {
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'Point',
-                coordinates: [longitude, latitude]
-              }
-            };
-          }
-        }).filter(Boolean) // Eliminamos elementos nulos
-      };
+        if (!isNaN(latitude) && !isNaN(longitude)) {
+          return {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'Point',
+              coordinates: [longitude, latitude]
+            }
+          };
+        }
+      }).filter(Boolean) // Eliminamos elementos nulos
+    };
 
-      // Añadimos los datos al mapa
-      map.on('load', function() {
-        map.addLayer({
-          id: 'points',
-          type: 'circle',
-          source: {
-            type: 'geojson',
-            data: geojson
-          },
-          paint: {
-            'circle-radius': 3,
-            'circle-color': '#FF0000'
-          }
-        });
+    // Añadimos los datos al mapa
+    map.on('load', function() {
+      map.addLayer({
+        id: 'points',
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: geojson
+        },
+        paint: {
+          'circle-radius': 3,
+          'circle-color': '#FF0000'
+        }
       });
     });
+  });
