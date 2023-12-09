0d31bbb320ee88adc7333bf6c76b8104d6f0019b
Francisco García <franciscogarcb@outlook.com>
2023-12-09 13:17:43 +0100

    Added first map

--- JavaScript/dotdensity1.js
+++ JavaScript/dotdensity1.js
@@ -1,40 +1,40 @@
 // Reemplaza 'TU_TOKEN_MAPBOX' con tu token de Mapbox
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmNpc2NvZ2FyY2IiLCJhIjoiY2xweDN5ZXMwMGxoNDJxbzl4ZTg4Mjg4aiJ9.zmi1_1GQonBXdbnESnCy6g';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmNpc2NvZ2FyY2IiLCJhIjoiY2xweDN5ZXMwMGxoNDJxbzl4ZTg4Mjg4aiJ9.zmi1_1GQonBXdbnESnCy6g';
 
 // Inicializamos el mapa en una ubicación específica (coordenadas del centro de Estados Unidos)
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/navigation-day-v1',
+  style: 'mapbox://styles/mapbox/streets-v11',
   center: [-98.5795, 39.8283],
   zoom: 3
 });
 
-  // Cargamos los datos desde el archivo CSV
-  fetch('../datasets/dot-density.csv')
-    .then(response => response.text())
-    .then(csvData => {
-      // Parseamos el CSV
-      var points = Papa.parse(csvData, { header: true }).data;
+/* // Cargamos los datos desde el archivo CSV
+fetch('../datasets/dot-density.csv')
+  .then(response => response.text())
+  .then(csvData => {
+    // Parseamos el CSV
+    var points = Papa.parse(csvData, { header: true }).data;
 
-      // Convertimos los puntos a GeoJSON
-      var geojson = {
-        type: 'FeatureCollection',
-        features: points.map(point => {
-          var latitude = parseFloat(point.latitude_coordinate);
-          var longitude = parseFloat(point.longitude_coordinate);
+    // Convertimos los puntos a GeoJSON
+    var geojson = {
+      type: 'FeatureCollection',
+      features: points.map(point => {
+        var latitude = parseFloat(point.latitude_coordinate);
+        var longitude = parseFloat(point.longitude_coordinate);
 
-          if (!isNaN(latitude) && !isNaN(longitude)) {
-            return {
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'Point',
-                coordinates: [longitude, latitude]
-              }
-            };
-          }
-        }).filter(Boolean) // Eliminamos elementos nulos
-      };
+        if (!isNaN(latitude) && !isNaN(longitude)) {
+          return {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'Point',
+              coordinates: [longitude, latitude]
+            }
+          };
+        }
+      }).filter(Boolean) // Eliminamos elementos nulos
+    };
 
     // Añadimos los datos al mapa
     map.on('load', function() {
@@ -47,8 +47,8 @@ var map = new mapboxgl.Map({
         },
         paint: {
           'circle-radius': 3,
-          'circle-color': '#04AA6D'
+          'circle-color': '#FF0000'
         }
       });
     });
-  })
+  }); */
