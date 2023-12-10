04b00fc8ddaa44f19827f56d8c4952fb97c70253
Francisco García <franciscogarcb@outlook.com>
2023-12-10 12:12:11 +0100

    added Dot Density maps

--- JavaScript/dotdensity2.js
+++ JavaScript/dotdensity2.js
@@ -1,47 +1,57 @@
-const svg = d3.select("#map")
-    .append("svg")
-    .attr("width", 800)
-    .attr("height", 800);
+mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmNpc2NvZ2FyY2IiLCJhIjoiY2xweDN5ZXMwMGxoNDJxbzl4ZTg4Mjg4aiJ9.zmi1_1GQonBXdbnESnCy6g';
 
-const projection = d3.geoAlbersUsa()
-    .scale(1000)
-    .translate([400, 300]);
+// Initialize map on USA
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/navigation-day-v1',
+  center: [-98.5795, 39.8283],
+  zoom: 3
+});
 
-const path = d3.geoPath().projection(projection);
+// Load CSV data using d3 library
+d3.csv('../datasets/dot-density2.csv', function (error, data) {
+  if (error) throw error;
 
-// Definir una escala de colores ordinal
-const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+  // Define a color scale for "common_name"
+  var colorScale = d3.scaleOrdinal()
+    .domain(data.map(function (d) { return d.common_name; }))
+    .range(d3.schemeCategory10); // You can choose a different color scheme
 
-// Load US map data
-d3.json("../datasets/us-states.json", function (us) {
-    // Dibujar los estados
-    svg.selectAll("path")
-        .data(us.features)
-        .enter().append("path")
-        .attr("d", path)
-        .attr("stroke", "#ccc")
-        .attr("stroke-width", 1)
-        .attr("fill", "white");
-
-    // Load GeoJSON data
-    d3.json("../datasets/dot-density2.geojson", function (geojson) {
-        // Dibujar puntos con colores según 'common_name'
-        svg.selectAll("circle")
-            .data(geojson.features)
-            .enter().append("circle")
-            .attr("cx", function (d) {
-                const coordinates = d.geometry.coordinates;
-                return projection([coordinates[0], coordinates[1]])[0];
-            })
-            .attr("cy", function (d) {
-                const coordinates = d.geometry.coordinates;
-                return projection([coordinates[0], coordinates[1]])[1];
-            })
-            .attr("r", 0.1)
-            .attr("fill", function (d) {
-                // Asignar color según 'common_name'
-                return colorScale(d.properties.name);
-            })
-            .attr("opacity", 0.7);
+  // Add points to the map
+  map.on('load', function () {
+    map.addSource('points', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: data.map(function (d) {
+          return {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [parseFloat(d.longitude_coordinate), parseFloat(d.latitude_coordinate)]
+            },
+            properties: {
+              common_name: d.common_name
+            }
+          };
+        })
+      }
     });
+
+    // Add a layer for each unique value in "common_name"
+    data.forEach(function (d) {
+      var layerId = 'point-' + d.common_name;
+      map.addLayer({
+        id: layerId,
+        type: 'circle',
+        source: 'points',
+        filter: ['==', 'common_name', d.common_name],
+        paint: {
+          'circle-color': colorScale(d.common_name),
+          'circle-radius': 5,
+          'circle-opacity': 0.7
+        }
+      });
+    });
+  });
 });
