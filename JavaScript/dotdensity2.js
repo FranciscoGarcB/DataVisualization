54118a3517d3ae97b2f41ffeb9b2c882f9dbdc57
Francisco García <franciscogarcb@outlook.com>
2023-12-10 16:45:45 +0100

    added map2

--- JavaScript/dotdensity2.js
+++ JavaScript/dotdensity2.js
@@ -1,57 +1,72 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmNpc2NvZ2FyY2IiLCJhIjoiY2xweDN5ZXMwMGxoNDJxbzl4ZTg4Mjg4aiJ9.zmi1_1GQonBXdbnESnCy6g';
+// Set up the SVG container
+var width = 800;
+var height = 600;
 
-// Initialize map on USA
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/navigation-day-v1',
-  center: [-98.5795, 39.8283],
-  zoom: 3
-});
+var svg = d3.select('#map')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
 
-// Load CSV data using d3 library
-d3.csv('../datasets/dot-density2.csv', function (error, data) {
-  if (error) throw error;
+// Load the US states GeoJSON
+d3.json('../datasets/us-states.json').then(function(us) {
+  // Load the CSV data
+  
+    // Create a color scale for common_name values
+    var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
-  // Define a color scale for "common_name"
-  var colorScale = d3.scaleOrdinal()
-    .domain(data.map(function (d) { return d.common_name; }))
-    .range(d3.schemeCategory10); // You can choose a different color scheme
+    // Create projection and path for the map
+    var projection = d3.geoAlbersUsa()
+      .translate([width / 2, height / 2])
+      .scale(1000);
 
-  // Add points to the map
-  map.on('load', function () {
-    map.addSource('points', {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: data.map(function (d) {
-          return {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [parseFloat(d.longitude_coordinate), parseFloat(d.latitude_coordinate)]
-            },
-            properties: {
-              common_name: d.common_name
-            }
-          };
-        })
-      }
-    });
+    var path = d3.geoPath().projection(projection);
 
-    // Add a layer for each unique value in "common_name"
-    data.forEach(function (d) {
-      var layerId = 'point-' + d.common_name;
-      map.addLayer({
-        id: layerId,
-        type: 'circle',
-        source: 'points',
-        filter: ['==', 'common_name', d.common_name],
-        paint: {
-          'circle-color': colorScale(d.common_name),
-          'circle-radius': 5,
-          'circle-opacity': 0.7
-        }
+    // Bind data to the map and draw state boundaries
+    svg.selectAll('path')
+      .data(us.features)
+      .enter().append('path')
+      .attr('d', path)
+      .style('fill', 'white')
+      .style('stroke', 'lightgray');
+
+  d3.csv('../datasets/dot-density2.csv').then(function(data) {
+    // Add dots for each row in the CSV
+    svg.selectAll('circle')
+      .data(data)
+      .enter().append('circle')
+      .attr('cx', function(d) {
+        return projection([+d.longitude_coordinate, +d.latitude_coordinate])[0];
+      })
+      .attr('cy', function(d) {
+        return projection([+d.longitude_coordinate, +d.latitude_coordinate])[1];
+      })
+      .attr('r', 1.5) // Adjust the radius as needed
+      .style('fill', function(d) {
+        return colorScale(d.common_name);
       });
-    });
+
+    // Add legend
+    var legend = svg.selectAll('.legend')
+      .data(colorScale.domain())
+      .enter().append('g')
+      .attr('class', 'legend')
+      .attr('transform', function(d, i) {
+        return 'translate(0,' + i * 20 + ')';
+      });
+
+    legend.append('rect')
+      .attr('x', width - 18)
+      .attr('width', 18)
+      .attr('height', 18)
+      .style('fill', colorScale);
+
+    legend.append('text')
+      .attr('x', width - 24)
+      .attr('y', 9)
+      .attr('dy', '.35em')
+      .style('text-anchor', 'end')
+      .text(function(d) {
+        return d;
+      }); 
   });
 });
